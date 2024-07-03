6693d21ae2d9d238032dd1f25410bfb0f02b844f
Andrii Antonenko <andriyantonenko3.16@gmail.com>
2024-07-03 22:46:55 +0300

    🔒️ Non-burnable

--- src/UahCoin.sol
+++ src/UahCoin.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.24;
 
 import { ConfirmedOwner } from "@chainlink/contracts/shared/access/ConfirmedOwner.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract UahCoin is ERC20, ERC20Burnable, ConfirmedOwner {
+contract UahCoin is ERC20, ConfirmedOwner {
   constructor(address _owner) ConfirmedOwner(_owner) ERC20("UahCoin", "UAHc") { }
 }
